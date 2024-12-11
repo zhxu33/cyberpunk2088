0991a241e3fae3587e69fb4a60ddc5b9974deac9
Xiuyuan Qi <xiuyuanqi799@gmail.com>
2024-12-10 18:21:16 -0800

    boss_ex1 initial design

--- cyberpunk2088/scripts/characters/enemy.gd
+++ cyberpunk2088/scripts/characters/enemy.gd
@@ -69,6 +69,8 @@ func take_damage(dmg:int) -> void:
 		Stats.coins += coin_reward
 		if self is BossSlime: # await death animation
 			await get_tree().create_timer(2).timeout
+		if self is BossEx1: # await death animation
+			await get_tree().create_timer(3.5).timeout
 		queue_free()
 	
 
