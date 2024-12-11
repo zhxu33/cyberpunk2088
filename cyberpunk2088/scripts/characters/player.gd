59f1e1b70905f1fba4926f435ed2dd12529674cb
Aaron Shan <shanhaoyuan820712@gmail.com>
2024-12-10 22:12:56 -0800

    Update player.gd

--- cyberpunk2088/scripts/characters/player.gd
+++ cyberpunk2088/scripts/characters/player.gd
@@ -57,8 +57,7 @@ func _physics_process(delta: float):
 		if return_time <= 0:
 			if (find_child("Teleport", true, false)):
 				find_child("Teleport", true, false).queue_free()
-			if (get_parent().find_child("Map", true, false)):
-				global_position = get_parent().find_child("Map", true, false).find_child("PlayerSpawn").global_position
+			global_position = get_parent().find_child("Map", true, false).find_child("PlayerSpawn").global_position
 	else:
 		return_time = 1.0
 		returning = false
@@ -75,7 +74,7 @@ func _physics_process(delta: float):
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			up_cmd.execute(self)
-		elif jump_amount < Stats.upgrades["Double Jump"]:
+		elif jump_amount < Stats.upgrades["Double Jump"] && velocity.y >= 0:
 			jump_amount += 1
 			# Move to doublejump animation
 			state_machine.start("multi_jump", true)
@@ -182,6 +181,7 @@ func unbind_player_input_commands():
 func player_reset(): 
 	_damaged = false
 	_dead = false
+	first_time = true 
 	animation_tree.active = true
 	jump_velocity = DEFAULT_JUMP_VELOCITY - Stats.upgrades["Jump Power"]*25
 	movement_speed = DEFAULT_MOVE_VELOCITY + Stats.upgrades["Movement Speed"]*20
